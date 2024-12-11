c1480063dc195b5be0a71f6f04cc43d43c8dc2a2
jineeds <jineeds94@gmail.com>
2024-12-11 10:09:38 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import Hello, { Counter, Hello2 } from '@/components/Hello';
+import ListTail from '@/components/list/ListTail';
+import { Button, Checkbox } from '@chakra-ui/react';
+
+export default function Home() {
+    return (
+        <>
+            <Button>dd</Button>
+        </>
+    );
+}
